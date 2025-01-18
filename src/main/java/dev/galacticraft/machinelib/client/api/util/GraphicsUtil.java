dc273c7fe5ed61c57dde358684dbe27f821b9d7d
Roelymole <roelymole@gmail.com>
2025-01-18 08:49:39 +0000

    fix: resolves #15

--- src/main/java/dev/galacticraft/machinelib/client/api/util/GraphicsUtil.java
+++ src/main/java/dev/galacticraft/machinelib/client/api/util/GraphicsUtil.java
@@ -22,11 +22,23 @@
 
 package dev.galacticraft.machinelib.client.api.util;
 
+import org.joml.Matrix4f;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.BufferUploader;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.blaze3d.vertex.VertexFormat;
+
 import net.fabricmc.fabric.api.transfer.v1.client.fluid.FluidVariantRendering;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariantAttributes;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FastColor;
 import net.minecraft.world.level.material.Fluids;
 
@@ -49,15 +61,37 @@ public final class GraphicsUtil {
             assert sprite != null;
         }
 
+        int tileWidth = 16;
+        int tileHeight = 16;
         int fluidHeight = (int) (((double) available / (double) capacity) * height);
         int startY = fillFromTop ? y : y + (height - fluidHeight);
+        int depth = startY + fluidHeight;
+        float u0 = sprite.getU0();
+        float v0 = sprite.getV0();
 
-        for (int splitX = 0; splitX < width; splitX += Math.min(width, 16)) {
-            int realWidth = Math.min(width - splitX, 16);
-            for (int splitY = startY; splitY < startY + fluidHeight; splitY += realWidth) {
-                graphics.blit(x + splitX, splitY, 0, realWidth, Math.min(realWidth, startY + fluidHeight - splitY), sprite, r, g, b, 1.0f);
+        for (int splitX = 0; splitX < width; splitX += tileWidth) {
+            int realWidth = Math.min(width - splitX, tileWidth);
+            for (int splitY = startY; splitY < depth; splitY += tileHeight) {
+                int realHeight = Math.min(depth - splitY, tileHeight);
+                float u1 = sprite.getU((float) realWidth / (float) tileWidth);
+                float v1 = sprite.getV((float) realHeight / (float) tileHeight);
+                innerBlit(graphics.pose(), sprite.atlasLocation(), x + splitX, x + splitX + realWidth, splitY, splitY + realHeight, 0, u0, u1, v0, v1, r, g, b, 1.0f);
             }
         }
+    }
+
+    private static void innerBlit(PoseStack poseStack, ResourceLocation resourceLocation, int x0, int x1, int y0, int y1, int z, float u0, float u1, float v0, float v1, float r, float g, float b, float a) {
+        RenderSystem.setShaderTexture(0, resourceLocation);
+        RenderSystem.setShader(GameRenderer::getPositionTexColorShader);
+        RenderSystem.enableBlend();
+        Matrix4f matrix = poseStack.last().pose();
+        BufferBuilder bufferBuilder = Tesselator.getInstance().begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX_COLOR);
+        bufferBuilder.addVertex(matrix, (float)x0, (float)y0, (float)z).setUv(u0, v0).setColor(r, g, b, a);
+        bufferBuilder.addVertex(matrix, (float)x0, (float)y1, (float)z).setUv(u0, v1).setColor(r, g, b, a);
+        bufferBuilder.addVertex(matrix, (float)x1, (float)y1, (float)z).setUv(u1, v1).setColor(r, g, b, a);
+        bufferBuilder.addVertex(matrix, (float)x1, (float)y0, (float)z).setUv(u1, v0).setColor(r, g, b, a);
+        BufferUploader.drawWithShader(bufferBuilder.buildOrThrow());
+        RenderSystem.disableBlend();
     }
 
     public static void highlightElement(GuiGraphics graphics, int left, int top, int x, int y, int width, int height, int color) {
