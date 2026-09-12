6050cd4fd9ab226f242b9665d03f4870456217c6
Roelymole <161453965+Roelymole@users.noreply.github.com>
2026-09-12 10:41:10 +0100

    feat: groundwork for oxygen detector (#27)
    * feat: allow faces to be overriden with base machine texture
    
    * chore(ci): use temporary address for maven to find the mojarn plugin
    
    * chore: document the new faceHasOverride method in the ConfiguredBlockEntity class
    
    * chore: bump version number

--- src/main/java/dev/galacticraft/machinelib/api/block/entity/ConfiguredBlockEntity.java
+++ src/main/java/dev/galacticraft/machinelib/api/block/entity/ConfiguredBlockEntity.java
@@ -315,6 +315,17 @@ public abstract class ConfiguredBlockEntity extends BaseBlockEntity implements R
         tag.putBoolean(Constant.Nbt.ACTIVE, this.active);
     }
 
+    /**
+     * Override this method to allow for alternative faces to be selected in the side config,
+     * such as to choose which sides of the oxygen detector have vents to test for the presence of oxygen.
+     *
+     * @param face the block face being checked.
+     * @return {@code true} if the face should allow the {@code OVERRIDE} resource type, {@code false} otherwise.
+     */
+    public boolean faceHasOverride(BlockFace face) {
+        return false;
+    }
+
     private IOFace @NotNull [] generateIOFaces() {
         IOFace[] faces = new IOFace[6];
         for (int i = 0; i < faces.length; i++) {
